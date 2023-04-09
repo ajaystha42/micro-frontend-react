f6b462ddb9feaaf567ab85aa21c4fea5b9f25fa7
Ajay Shrestha <er.ajaystha@gmail.com>
2023-04-09 19:59:22 -0400

    module federation for header & footer

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 
+import Footer from "home/Footer";
+const Header = React.lazy(() => import("home/Header"));
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: pdp</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="text-3xl mx-auto max-w-6xl">
+      <Suspense fallback={<div className="pt-2 text-center">Loading...</div>}>
+        <Header />
+      </Suspense>
+      {/* check network tab for js section */}
+      {/* {!showHeader && (
+        <button className="text-3xl" onClick={() => setShowHeader(true)}>
+          Show the header
+        </button>
+      )} */}
+      <div className="my-10">PDP Page COntent</div>
+      <Footer />
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
