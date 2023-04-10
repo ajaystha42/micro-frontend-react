7ef0ace319a4eafc9e44db5e01a88e9ae80bbac3
Ajay Shrestha <er.ajaystha@gmail.com>
2023-04-09 22:15:54 -0400

    nest project setup

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,25 +1,20 @@
 import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 
+import Header from "home/Header";
 import Footer from "home/Footer";
-const Header = React.lazy(() => import("home/Header"));
+// const Header = React.lazy(() => import("home/Header"));
 import "./index.scss";
+import SafeComponent from "./SafeComponent";
 
-const App = () => {
-  return (
-    <div className="text-3xl mx-auto max-w-6xl">
-      <Suspense fallback={<div className="pt-2 text-center">Loading...</div>}>
-        <Header />
-      </Suspense>
-      {/* check network tab for js section */}
-      {/* {!showHeader && (
-        <button className="text-3xl" onClick={() => setShowHeader(true)}>
-          Show the header
-        </button>
-      )} */}
-      <div className="my-10">PDP Page COntent</div>
-      <Footer />
-    </div>
-  );
-};
+const App = () => (
+  <div className="text-3xl mx-auto max-w-6xl">
+    <SafeComponent>
+      <Header />
+    </SafeComponent>
+    <div className="my-10">PDP Page COntent</div>
+    <Footer />
+  </div>
+);
+
 ReactDOM.render(<App />, document.getElementById("app"));
